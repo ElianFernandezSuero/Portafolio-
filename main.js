4bc932bafd7825d733562e8dd2d14ba438038e98
Yucaflu <82125953+Yucaflu@users.noreply.github.com>
2021-06-08 18:37:44 -0400

    Primera actualización del portafolio

---
+++ main.js
@@ -0,0 +1,6 @@
+const barbtn = document.querySelector(".bar-btn");
+const ItemsUl = document.querySelector(".Items-ul");
+barbtn.addEventListener("click", () => { 
+    ItemsUl.classList.toggle("Items-ul-visible")
+ });
+
